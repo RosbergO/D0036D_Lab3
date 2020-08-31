6797f2c1ff26043ff748dfb088928b108a19d6eb
hotpucko <stn.jonsson@gmail.com>
2020-08-31 16:03:29 +0200

    Added colors

---
+++ src/Color.java
@@ -0,0 +1,12 @@
+public class Color
+{
+    int red;
+    int green;
+    int blue;
+    public Color(int red, int green, int blue)
+    {
+        this.red = red;
+        this.green = green;
+        this.blue = blue;
+    }
+};
--- src/main.java
+++ src/main.java
@@ -1,2 +1,23 @@
 public class main {
+    public static void main(String[] args) {
+
+        int[][] gameBoard = new int[201][201]; //All values are default 0
+        Color[] colors = {
+                new Color(255, 0, 0),//red
+                new Color(255, 255, 0),//yellow
+                new Color(255, 0 ,255),//pink
+                new Color(255, 255, 255),//black
+                new Color(0, 255, 0),//Green
+                new Color(0, 255, 255),//Cyan
+                new Color(0, 0, 255),//Blue
+                new Color(0, 0, 0),//White
+                new Color(128, 0, 255),//Purple
+                new Color(128, 128, 128)//Grey
+        };
+
+
+        System.out.println(gameBoard[2][3]);
+    }
 }
+
+
